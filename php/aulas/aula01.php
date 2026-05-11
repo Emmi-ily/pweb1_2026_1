31ac0c3bcd8a9ce4da231324ccd656a92d34bb15
Emily Menezes <emily.m1@aluno.ifsc.edu.br>
2026-05-11 15:47:52 -0300

    aaa
    faltava commitar

--- php/aulas/aula01.php
+++ php/aulas/aula01.php
@@ -18,7 +18,7 @@
         }
 
 
-        //print_r($notas);
+        //print_r($notas); (imprime os dados do vetor notas, é meio q um echo para vetores)
         $notas = [5, 7, 10, 9];
         echo "<br>"; //quebrar linha
         for($i = 0; $i < count (value: $notas); $i++){
@@ -42,8 +42,23 @@
 
 
         echo "<br>";
-        $carro = ['modelo' => "Mustang", 'cor' => "Branco", 'ano' => 2026];
-        echo $carro['modelo'] . "-" . $carro['cor'];
+
+        //matriz
+        $carros = [
+            ['modelo' => "Mustang", 'cor' => "Branco", 'ano' => 2026], //vetor(unidimenssional) //quando chama o indice modelo, ele retorna mustang
+            ['modelo' => "fusca", 'cor' => "azul", 'ano' => 1973],     // matriz (tem coluna e linha pra refernciar)
+            ['modelo' => "brasilia", 'cor' => "amarela", 'ano' => 1969],
+        ];
+
+        echo $carros[0]['modelo'] . "-" . $carros[0]['cor'];
+        echo "<br>";
+
+        // para matriz
+        foreach($carros as $indice => $carro) {
+                echo $indice + 1;
+                echo "modelo: " . $carro['modelo'] . "<br>" . "ano" . $carro['ano'];
+                echo "<br>";
+            }
     ?>
 
     <p> Meu site <?=$carro['modelo'] . " - Ano: " . $carro['ano'] ?> </p> 
---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+include './header.php';
+?>
+
+
+<a href="./aulas./aula03_form.php" class="btn btn-primary">Formulario aluno</a>
+
+<?php
+include './footer.php';
+?>
